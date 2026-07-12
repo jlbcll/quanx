1e09017132ccc1ecf904ad91af02bbb7053192a7
Azuma Seren <119632229+jlbcll@users.noreply.github.com>
2026-07-13 02:06:29 +0800

    Update chsi.js

--- chsi.js
+++ chsi.js
@@ -1,67 +1,31 @@
-/*
- 学信档案 detail.action 修改脚本
-*/
-
 let body = $response.body;
 
-// 学校
-body = body.replace(
-  /"yxmc":"[^"]*"/,
-  '"yxmc":"清华大学"'
-);
+// 先处理普通字符串
+body = body.replace(/"yxmc":"[^"]*"/g,
+'"yxmc":"清华大学"');
 
-// 专业
-body = body.replace(
-  /"zymc":"[^"]*"/,
-  '"zymc":"核科学与技术"'
-);
+body = body.replace(/"zymc":"[^"]*"/g,
+'"zymc":"核科学与技术"');
 
-// 层次
-body = body.replace(
-  /"cc":"[^"]*"/,
-  '"cc":"博士研究生"'
-);
+body = body.replace(/"cc":"[^"]*"/g,
+'"cc":"博士研究生"');
 
-// 学习形式
-body = body.replace(
-  /"xxxs":"[^"]*"/,
-  '"xxxs":"全日制"'
-);
+body = body.replace(/"xxxs":"[^"]*"/g,
+'"xxxs":"全日制"');
 
-// 学制（空白）
-body = body.replace(
-  /"xz":"[^"]*"/,
-  '"xz":""'
-);
+body = body.replace(/"xz":"[^"]*"/g,
+'"xz":""');
 
-// 学历类别
-body = body.replace(
-  /"xllb":"[^"]*"/,
-  '"xllb":"普通高等教育"'
-);
+body = body.replace(/"xjzt":"[^"]*"/g,
+'"xjzt":"在籍(注册学籍)"');
 
-// 学籍状态
-body = body.replace(
-  /"xjzt":"[^"]*"/,
-  '"xjzt":"在籍(注册学籍)"'
-);
+body = body.replace(/"byrq":"[^"]*"/g,
+'"byrq":"2100年07月15日"');
 
-// 入学日期（保持不变，如需修改可添加）
-// body = body.replace(/"rxrq":"[^"]*"/,'"rxrq":"2018年09月01日"');
-
-// 离校日期
-body = body.replace(
-  /"byrq":"[^"]*"/,
-  '"byrq":"2100年07月15日"'
-);
-
-// 民族
-// body = body.replace(/"mz":"[^"]*"/,'"mz":"汉族"');
-
-// 姓名
-// body = body.replace(/"xm":"[^"]*"/,'"xm":"测试姓名"');
+body = body.replace(/"xllb":"[^"]*"/g,
+'"xllb":"普通高等教育"');
 
 
 $done({
-  body: body
+ body
 });
