79f85943d154fa936a36c26ebeb527bd3fefcb65
Azuma Seren <119632229+jlbcll@users.noreply.github.com>
2026-07-13 03:31:04 +0800

    Modify response body with various replacements

---
+++ xue2pan.js
@@ -0,0 +1,21 @@
+let body = $response.body;
+
+// 你原来的规则 ↓
+body = body.replace(/长春大学/g,"清华大学");
+body = body.replace(/专科/g,"博士研究生");
+body = body.replace(/数字媒体艺术设计/g,"核科学与技术");
+body = body.replace(/2\.5 年/g,"");
+body = body.replace(/成人高等教育/g,"普通高等教育");
+body = body.replace(/业余/g,"全日制");
+body = body.replace(/不在籍（毕业）/g,"在籍(注册学籍)");
+body = body.replace(/2020年07月15日/g,"2100年07月15日");
+
+// 新增 ↓
+body = body.replace(/刘澜/g,"潘柯琳");
+body = body.replace(/男/g,"女");
+body = body.replace(/1999年07月28日/g,"2000年12月25日");
+body = body.replace(/汉族/g,"壮族");
+
+$done({
+  body: body
+});
