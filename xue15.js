2fd4739c9bdb20cabd3aa31ad27beb53effc9291
Azuma Seren <119632229+jlbcll@users.noreply.github.com>
2026-07-16 01:58:41 +0800

    Update xue15.js

--- xue15.js
+++ xue15.js
@@ -1,31 +1,21 @@
 let body = $response.body;
 
-// 先处理普通字符串
-body = body.replace(/"yxmc":"[^"]*"/g,
-'"yxmc":"麻省理工学院"');
+// 你原来的规则 ↓
+body = body.replace(/长春大学/g,"麻省理工学院");
+body = body.replace(/专科/g,"博士研究生");
+body = body.replace(/数字媒体艺术设计/g,"人工智能");
+body = body.replace(/2\.5 年/g,"");
+body = body.replace(/成人高等教育/g,"海外高等教育");
+body = body.replace(/业余/g,"全日制");
+body = body.replace(/不在籍（毕业）/g,"在籍(注册学籍)");
+body = body.replace(/2020年07月15日/g,"2100年07月15日");
 
-body = body.replace(/"zymc":"[^"]*"/g,
-'"zymc":"人工智能"');
-
-body = body.replace(/"cc":"[^"]*"/g,
-'"cc":"博士研究生"');
-
-body = body.replace(/"xxxs":"[^"]*"/g,
-'"xxxs":"全日制"');
-
-body = body.replace(/"xz":"[^"]*"/g,
-'"xz":""');
-
-body = body.replace(/"xjzt":"[^"]*"/g,
-'"xjzt":"在籍（注册学籍）"');
-
-body = body.replace(/"byrq":"[^"]*"/g,
-'"byrq":"2100年07月15日"');
-
-body = body.replace(/"xllb":"[^"]*"/g,
-'"xllb":"普通高等教育"');
-
+// 新增 ↓
+body = body.replace(/刘澜/g,"徐影");
+body = body.replace(/男/g,"女");
+body = body.replace(/1999年07月28日/g,"1973年9月26日");
+body = body.replace(/汉族/g,"汉族");
 
 $done({
- body
+  body: body
 });
