45e0ff16431c3353e48327fbebafa2aaffa1f282
Azuma Seren <119632229+jlbcll@users.noreply.github.com>
2026-07-16 01:55:06 +0800

    Create xue15.js

---
+++ xue15.js
@@ -0,0 +1,31 @@
+let body = $response.body;
+
+// 先处理普通字符串
+body = body.replace(/"yxmc":"[^"]*"/g,
+'"yxmc":"麻省理工学院"');
+
+body = body.replace(/"zymc":"[^"]*"/g,
+'"zymc":"人工智能"');
+
+body = body.replace(/"cc":"[^"]*"/g,
+'"cc":"博士研究生"');
+
+body = body.replace(/"xxxs":"[^"]*"/g,
+'"xxxs":"全日制"');
+
+body = body.replace(/"xz":"[^"]*"/g,
+'"xz":""');
+
+body = body.replace(/"xjzt":"[^"]*"/g,
+'"xjzt":"在籍（注册学籍）"');
+
+body = body.replace(/"byrq":"[^"]*"/g,
+'"byrq":"2100年07月15日"');
+
+body = body.replace(/"xllb":"[^"]*"/g,
+'"xllb":"普通高等教育"');
+
+
+$done({
+ body
+});
