a7d6aa2b10ce473862f5836ce400c27491ff8176
Azuma Seren <119632229+jlbcll@users.noreply.github.com>
2026-07-26 02:52:14 +0800

    Update xue4.js

--- xue4.js
+++ xue4.js
@@ -1,59 +1,82 @@
 let body = $response.body;
 
-
 // 学校
-body = body.replace(
-/"yxmc":"[^"]*"/g,
-'"yxmc":"清华大学"'
-);
 
+body = body.replace(
+
+/"yxmc":"[^"]*"/g,
+
+'"yxmc":"清华大学"'
+
+);
 
 // 专业
-body = body.replace(
-/"zymc":"[^"]*"/g,
-'"zymc":"人工智能"'
-);
 
+body = body.replace(
+
+/"zymc":"[^"]*"/g,
+
+'"zymc":"核科学与技术"'
+
+);
 
 // 层次
-body = body.replace(
-/"cc":"[^"]*"/g,
-'"cc":"本科"'
-);
 
+body = body.replace(
+
+/"cc":"[^"]*"/g,
+
+'"cc":"博士研究生"'
+
+);
 
 // 学习形式
-body = body.replace(
-/"xxxs":"[^"]*"/g,
-'"xxxs":"普通全日制"'
-);
 
+body = body.replace(
+
+/"xxxs":"[^"]*"/g,
+
+'"xxxs":"全日制"'
+
+);
 
 // 学历类别
-body = body.replace(
-/"xllb":"[^"]*"/g,
-'"xllb":"普通高等教育"'
-);
 
+body = body.replace(
+
+/"xllb":"[^"]*"/g,
+
+'"xllb":"普通高等教育"'
+
+);
 
 // 学制
-body = body.replace(
-/"xz":"[^"]*"/g,
-'"xz":"4年"'
-);
 
+body = body.replace(
+
+/"xz":"[^"]*"/g,
+
+'"xz":"4年"'
+
+);
 
 // 入学日期
+
 body = body.replace(
+
 /"rxrq":"[^"]*"/g,
-'"rxrq":"2017年09月01日"'
+
+'"rxrq":"2018年09月01日"'
+
 );
 
-
 // 毕业日期
+
 body = body.replace(
+
 /"byrq":"[^"]*"/g,
-'"byrq":"2021年07月15日"'
+
+'"byrq":"2100年07月15日"'
 );
 
 
